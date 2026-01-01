a5ea3a8ad68c412901fb2f508af11229bd948d4a
Hamid Yosefsei <hamidyosefzai1@gmail.com>
2026-01-02 00:36:07 +0100

    Fix: Verwende bzip2 statt unbzip2-stream (zuverlässiger auf Linux)

--- src/updater/downloader.js
+++ src/updater/downloader.js
@@ -95,9 +95,14 @@ class Downloader {
                 logger.info('Windows erkannt, verwende unbzip2-stream');
                 useExternalTool = false;
             } else {
-                // Linux/Mac: Verwende immer unbzip2-stream (zuverlässiger)
-                logger.info('Verwende unbzip2-stream (zuverlässiger als bzip2)');
-                useExternalTool = false;
+                // Linux/Mac: bzip2 (zuverlässiger als unbzip2-stream)
+                if (this._commandExists('bzip2')) {
+                    command = 'bzip2';
+                    args = ['-d', '-c', sourcePath];
+                } else {
+                    logger.info('bzip2 nicht gefunden, verwende unbzip2-stream');
+                    useExternalTool = false;
+                }
             }
 
             // Fallback: Node.js Stream-basiert
